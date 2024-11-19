df3ea2c89126ca9b24a499afa60516f93ee6f393
Aster Chang <aychang1@cps.edu>
2024-11-19 12:11:41 -0600

    11/19/24

--- ControlFlowLabPart3/main.swift
+++ ControlFlowLabPart3/main.swift
@@ -132,9 +132,16 @@ print("***PROBLEM 3.2 : DINNER TIME")
  Another friend brings up a restaurant she thinks will fit both of your criteria. This restaurant's attributes are represented by a few constants below. Write an if-else statement that will print "Let's go!" if the restaurant's attributes match the group's dietary requirements, and otherwise will print "Sorry, we'll have to think of somewhere else."
  HINT: Create three boolean constants of hasFish, hasPizza, & hasVegan that are true. Then write your if-else statements.
  */
+let hasFish = true
+let hasPizza = true
+let hasVegan = true
 
-
-
+if (hasFish || hasPizza && hasVegan) {
+    print("Let's go!")
+}
+else {
+    print("Sorry, we'll have to think of somewhere else.")
+}
 print("\n")
 print("***PROBLEM 3.3 : COMBINING ALL LOGIC")
 // Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant isNiceWeather that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice.
